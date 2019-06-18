aca244d59390a1c59dfaf5b0ebc2de6057ec9cdd
Richard Kuesters <rkuesters@gmail.com>
2019-06-18 11:56:25 -0300

    Bump version: 0.2.2 → 0.2.3

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'middle'
 year = '2018'
 author = 'Richard Kuesters'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.2.2'
+version = release = '0.2.3'
 
 pygments_style = 'default'
 templates_path = ['.']
